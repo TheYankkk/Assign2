de22d2445f4d0be7e9a19444a1c292b075d8a1c6
Bob Liu <527080292@qq.com>
2022-03-28 04:39:48 +0800

    a small demo of DT and its visualization

--- AssignDT.py
+++ AssignDT.py
@@ -1,1 +1,25 @@
 #test
+import numpy as np
+from sklearn import tree
+x=np.array([[0,0],[1,0],[0,0],[1,1],[0,1],[1,1],[1,1],[1,0]])
+y=np.array([1,0,1,0,1,0,1,1])
+clf=tree.DecisionTreeClassifier(criterion='gini')
+clf.fit(x,y)
+
+
+import graphviz
+#graphv = graphviz.Source(dot_data)
+
+
+#print the decision tree in a pdf file
+#from sklearn.externals.six import StringIO
+from six import StringIO
+import pydot
+
+dot_data = StringIO()
+tree.export_graphviz(clf, out_file=dot_data,
+                         filled=True, rounded=True,
+                         special_characters=True
+                    )
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph[0].write_pdf("test.pdf")
